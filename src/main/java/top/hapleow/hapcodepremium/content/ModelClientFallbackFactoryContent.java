bd52295a7b3f507d13a8faa5c918edc223dcb63a
wuyulin <hapleow@gmail.com>
2021-04-29 17:41:48 +0800

    DTO

---
+++ src/main/java/top/hapleow/hapcodepremium/content/ModelClientFallbackFactoryContent.java
@@ -0,0 +1,16 @@
+package top.hapleow.hapcodepremium.content;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * API的内容对象
+ */
+@Data
+@Component
+@ConfigurationProperties(prefix = "model-client-fallback-factory")
+public class ModelClientFallbackFactoryContent extends AbstractContent {
+
+
+}
